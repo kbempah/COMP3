696a10dbd32593eaf3e7b9c33b469b6facecbd34
Kwadwo Bempah <kwadwoasiedu@bempah.com>
2023-09-10 15:11:51 -0400

    practiced input validation with the cin object

---
+++ wk1/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <climits>
+
+using namespace std;
+
+void clear_keyboard_buffer();
+
+int main(void) {
+    int x;
+    
+    cout << "Enter a value for x that is non-negative > ";
+    cin >> x;
+
+    while (cin.fail() || x < 0) {
+        clear_keyboard_buffer();
+        cout << "Please enter a value that is non-negative > ";
+        cin >> x;
+    }
+    clear_keyboard_buffer();
+    
+    cout << "The final value is " << x << "\n";
+    return 0;
+}
+
+void clear_keyboard_buffer() {
+    cin.clear();
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+}
